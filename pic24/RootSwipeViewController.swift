bace5fce6dbab82f2f754eb54b25f90458c71ec9
Mohammad Al-Ahdal <malahdal@ualberta.ca>
2017-10-07 20:52:44 -0600

    create RootViewController

---
+++ pic24/RootSwipeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootSwipeViewController.swift
+//  pic24
+//
+//  Created by Mohammad Al-Ahdal on 2017-10-07.
+//  Copyright © 2017 Mohammad Al-Ahdal. All rights reserved.
+//
+
+import UIKit
+
+var cameraPage:CameraPage = CameraPage();
+var homePage:HomePage = HomePage();
+var notificationsPage:NotificationsPage = <#value#>
+
+
+class RootSwipeViewController : UIViewController {
+    
+    
+    
+}
