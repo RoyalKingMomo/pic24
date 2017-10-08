238f6fc4c5c223413a470cfe09dca2724752b808
Mohammad Al-Ahdal <malahdal@ualberta.ca>
2017-10-08 01:38:05 -0600

    make basic login functionality

--- pic24/LoginSignUpViewController.swift
+++ pic24/LoginSignUpViewController.swift
@@ -7,7 +7,25 @@
 //
 
 import UIKit
+var i = 1;
 
 class LoginSignUpViewController: UIViewController {
     //Create login
+    @IBOutlet weak var loginButton: CustomButton!
+    @IBOutlet weak var signUpButton: CustomButton!
+    
+    override func viewDidLoad() {
+        self.hideKeyboardWhenTappedAround();
+        self.loginButton.onPress = {self.performSegue(withIdentifier: "authSuccess", sender: self)}
+    }
+    
+}
+
+class CustomButton: UIView {
+    
+    var onPress: () -> Void  = {};
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        onPress()
+    }
 }
